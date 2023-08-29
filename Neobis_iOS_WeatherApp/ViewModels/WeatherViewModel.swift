83e1cb53cd725639b539c190ddc9ccbfd0e6a936
soronbekov-a-auca-2021 <soronbekov_a@auca.kg>
2023-08-29 20:24:19 +0600

    WeatherApp

---
+++ Neobis_iOS_WeatherApp/ViewModels/WeatherViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  WeatherViewModel.swift
+//  Neobis_iOS_WeatherApp
+//  Created by Askar Soronbekov
+
+import Foundation
+
+protocol WeatherViewModelType {
+    var didTapSearch: (() -> Void)? { get set }
+    var updateSearch: ((Welcome) -> Void)? { get set }
+    
+    func fetchWeatherData(for cityName: String)
+}
+
+class WeatherViewModel: WeatherViewModelType {
+    private var weatherService: WeatherService
+    private(set) var weatherData: Welcome? {
+        didSet {
+            bindWeatherViewModelToController?()
+        }
+    }
+    
+    var bindWeatherViewModelToController: (() -> Void)?
+    var didTapSearch: (() -> Void)?
+    var updateSearch: ((Welcome) -> Void)?
+    
+    init() {
+        self.weatherService = WeatherService()
+        fetchWeatherData(for: "Bishkek")
+    }
+    
+    func fetchWeatherData(for cityName: String) {
+        weatherService.fetchWeather(for: cityName) { [weak self] weatherData in
+            self?.weatherData = weatherData
+            self?.updateSearch?(weatherData)
+        }
+    }
+}
